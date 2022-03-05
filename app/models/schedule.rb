6e796089d2afa0a9fc7a635e615c362135720fbc
Bikash Shrestha <bikashshrestha@bajratechnologies.com>
2022-03-05 23:30:01 +0545

    feat: add validations to schedule and initialize

--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -1,2 +1,12 @@
 class Schedule < ApplicationRecord
+  belongs_to :room
+
+  validates :title, presence: true, length: { minimum: 5 }
+  validates_presence_of :date, :start_time, :end_time
+
+  after_initialize do
+    start_time = Time.parse(self.start_time.to_s)
+    self.start_time = start_time.strftime('%H:%M %p')
+    self.title = title.titleize
+  end
 end
