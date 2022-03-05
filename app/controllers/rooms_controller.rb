0e9acfd3e87b5299e356c7826df0a61ab029c2d9
Bikash Shrestha <bikashshrestha@bajratechnologies.com>
2022-03-05 17:40:47 +0545

    feat(room): update destroy and show room details based on role

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,10 +1,15 @@
 class RoomsController < ApplicationController
   before_action :authenticate_user!
   # before_action :is_admin?, only: %i[create]
+  before_action :find_room, only: %i[show update destroy ]
 
   def index
     rooms = Room.all
     render json: rooms, status: :ok
+  end
+
+  def show
+    render json: @room, status: :ok
   end
 
   def create
@@ -12,9 +17,27 @@ class RoomsController < ApplicationController
       room = Room.new(room_params)
       room.user_id = current_user.id
       room.save
-      render json: { messages: 'Room Created Successfully', room: room }, status: :ok
+      render json: { messages: 'Room Created Successfully.', room: room }, status: :created
     else
-      render json: { message: 'You are not allowed to create room' }, status: :unauthorized
+      render json: { message: 'You are not allowed to create room.' }, status: :unauthorized
+    end
+  end
+
+  def update
+    if is_admin?
+      @room.update(room_params)
+      render json: { meassage: 'Record Updated.', room: @room}, status: :ok
+    else
+      render json: { message: 'You are not allowed to update this record.' }, status: :unauthorized
+    end
+  end
+
+  def destroy
+    if is_admin?
+      @room.destroy
+      render json: { message: 'Record Deleted.' }, status: :ok
+    else
+      render json: { message: 'You are not allowed to remove this record.' }, status: :unauthorized
     end
   end
 
@@ -25,7 +48,11 @@ class RoomsController < ApplicationController
   end
 
   def is_admin?
-    p current_user
+    # p current_user
     current_user.admin?
+  end
+
+  def find_room 
+    @room = Room.find(params[:id]) rescue { message: 'record not found.' }
   end
 end
