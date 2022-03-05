1392173d4de1524d84c7a9af9667c4708cfc5348
Sanju Basnet <sanjubasnet@bajratechnologies.com>
2022-03-05 10:36:30 +0545

    feat: routes for devise user

--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  devise_for :users, defaults: { formats: :json },
+  controllers: {
+    sessions: 'users/sessions',
+    registrations: 'users/registrations'
+  }
+  get '/welcome-msg', to: 'welcomes#show'
 end
