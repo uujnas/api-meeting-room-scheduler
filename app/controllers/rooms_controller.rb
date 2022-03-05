ba1630e3b6d301a4d8deb17c5aed67eb8d2b6b73
Bikash Shrestha <bikashshrestha@bajratechnologies.com>
2022-03-05 16:16:41 +0545

    feat(room): create rooms -admin only

--- app/controllers/rooms_controller.rb
+++ app/controllers/rooms_controller.rb
@@ -1,9 +1,31 @@
 class RoomsController < ApplicationController
   before_action :authenticate_user!
+  # before_action :is_admin?, only: %i[create]
 
   def index
     rooms = Room.all
-    render json: rooms
+    render json: rooms, status: :ok
   end
 
+  def create
+    if is_admin?
+      room = Room.new(room_params)
+      room.user_id = current_user.id
+      room.save
+      render json: { messages: 'Room Created Successfully', room: room }, status: :ok
+    else
+      render json: { message: 'You are not allowed to create room' }, status: :unauthorized
+    end
+  end
+
+  private
+
+  def room_params
+    params.require(:room).permit(:desc)
+  end
+
+  def is_admin?
+    p current_user
+    current_user.admin?
+  end
 end
