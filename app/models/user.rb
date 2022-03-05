38797630322c05313929cbc31630483250b03488
Sanju Basnet <sanjubasnet@bajratechnologies.com>
2022-03-05 11:34:22 +0545

    feat: user role through enum

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,6 @@
 class User < ApplicationRecord
+  enum role: %i[admin participants]
+
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable,
