e872fd2c74197c1dfcaca223fb055bb5dcb9ccce
Bikash Shrestha <bikashshrestha@bajratechnologies.com>
2022-03-06 15:35:35 +0545

    feat: add validation to meeting and update schedule start & end time formats

--- app/models/meeting.rb
+++ app/models/meeting.rb
@@ -2,4 +2,10 @@ class Meeting < ApplicationRecord
   belongs_to :room
 
   has_one :schedule, dependent: :destroy
+
+  validates :subject, presence: true, length: { minimum: 5 }
+
+  before_save do
+    self.subject = self.subject.titleize
+  end
 end
--- app/models/schedule.rb
+++ app/models/schedule.rb
@@ -6,5 +6,7 @@ class Schedule < ApplicationRecord
   after_initialize do
     start_time = Time.parse(self.start_time.to_s)
     self.start_time = start_time.strftime('%H:%M %p')
+    end_time = Time.parse(self.end_time.to_s)
+    self.end_time = end_time.strftime('%H:%M %p')
   end
 end
