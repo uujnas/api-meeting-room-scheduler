5a0b408c850314132edd541db0db2c81b4547e61
Bikash Shrestha <bikashshrestha@bajratechnologies.com>
2022-03-08 12:28:46 +0545

    fix: remove unused controller 'welcome'

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,15 @@
 Rails.application.routes.draw do
   get '/rooms/meetings', to: 'meetings#all_meetings'
-  
+
   resources :rooms do
     resources :meetings do
       resources :schedules
     end
   end
 
-
   devise_for :users, defaults: { formats: :json },
   controllers: {
     sessions: 'users/sessions',
     registrations: 'users/registrations'
   }
-  get '/welcome-msg', to: 'welcomes#show'
 end
